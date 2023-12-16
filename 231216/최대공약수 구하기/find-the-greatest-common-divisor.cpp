5102275af322ed02df5ab6e44b0ec93b2cf890ba
Account-LJY <138774652+Account-LJY@users.noreply.github.com>
2023-12-16 16:30:55 +0900

    Title: 최대공약수 구하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231216/최대공약수 구하기/find-the-greatest-common-divisor.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int gcd(int a, int b) {
+    if (b == 0) {
+        return a;
+    }
+    int c = a % b;
+    return gcd(b, c);
+}
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n, m;
+    cin >> n >> m;
+    cout << gcd(n, m);
+    return 0;
+}
