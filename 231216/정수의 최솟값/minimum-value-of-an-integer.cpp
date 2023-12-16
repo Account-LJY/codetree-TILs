12dc8a1ac8e4ad29440f4e5c53e64d4c9e33d98f
Account-LJY <138774652+Account-LJY@users.noreply.github.com>
2023-12-16 20:12:33 +0900

    Title: 정수의 최솟값, Time: 8ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231216/정수의 최솟값/minimum-value-of-an-integer.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int aaa(int a, int b, int c) {
+  int min = a;
+  if (min > b) {
+    min = b;
+  }
+  if (min > c) {
+    min = c;
+  }
+  return min;
+}
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a, b, c;
+    cin >> a >> b >> c;
+    cout << aaa(a, b, c);
+    return 0;
+}
