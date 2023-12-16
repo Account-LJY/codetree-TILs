6901cead86351fec310ce67c9f4d2c621f47cabe
Account-LJY <138774652+Account-LJY@users.noreply.github.com>
2023-12-16 20:06:48 +0900

    Title: 1부터 특정 수까지의 합, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 231216/1부터 특정 수까지의 합/sum-from-1-to-a-certain-number.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int aaa(int a) {
+    int sum = 0;
+    int finish = a;
+    for (int i = 1; i <= finish; i++) {
+        sum += i;
+    }
+    sum %= 10;
+    return sum;
+}
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    cin >> n;
+    int result = aaa(n);
+    cout << result;
+    return 0;
+}
