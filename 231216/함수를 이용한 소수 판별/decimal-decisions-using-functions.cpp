e9717bb80df576fcec90da40103191660d63e421
Account-LJY <138774652+Account-LJY@users.noreply.github.com>
2023-12-16 21:15:30 +0900

    Title: 함수를 이용한 소수 판별, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 231216/함수를 이용한 소수 판별/decimal-decisions-using-functions.cpp
+++ 231216/함수를 이용한 소수 판별/decimal-decisions-using-functions.cpp
@@ -2,6 +2,9 @@
 using namespace std;
 
 bool isPrime(int a) {
+    if (a == 1) {
+        return false;
+    }
     for(int i = 2; i <= a / 2; i++) {
         if (a % i == 0) {
             return false;
