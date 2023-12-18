4960446f2dfdda4609612925371250f86f5ad116
Account-LJY <138774652+Account-LJY@users.noreply.github.com>
2023-12-18 16:45:15 +0900

    Title: 함수를 이용한 연속부분수열 여부 판단하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 231218/함수를 이용한 연속부분수열 여부 판단하기/to-determine-whether-a-continuous-subsequence-is-made-using-a-function.cpp
+++ 231218/함수를 이용한 연속부분수열 여부 판단하기/to-determine-whether-a-continuous-subsequence-is-made-using-a-function.cpp
@@ -4,33 +4,37 @@ using namespace std;
 
 bool judge(vector<int> a, vector<int> b) {
     int index = -1;
-    for (int i = 0; i < b.size(); i++) {
-        int target = b[i];
-        for (int j = 0; j < a.size(); j++) {
-            if (a[j] == target) {
-                if (index == j - 1) {
-                    return true;
-                }
-                index = j;
-                continue;
-            }
+    for (int i = 0; i < a.size(); i++) {
+        if (a[i] == b[0]) {
+            index = i;
+            break;
         }
     }
-    return false;
+    if (index == -1) {
+        return false;
+    }
+    int f = 1;
+    for (int i = index + 1; i < index + b.size(); i++) {
+        if (a[i] != b[f]) {
+            return false;
+        }
+        f++;
+    }
+    return true;
 } 
 
 int main() {
     // 여기에 코드를 작성해주세요.
     int n1, n2;
     cin >> n1 >> n2;
-    vector<int> A(n1);
-    vector<int> B(n2);
+    vector<int> A;
+    vector<int> B;
     int x;
     for (int i = 0; i < n1; i++) {
         cin >> x;
         A.push_back(x);
     }
-    for (int i = 0; i < n1; i++) {
+    for (int i = 0; i < n2; i++) {
         cin >> x;
         B.push_back(x);
     }
