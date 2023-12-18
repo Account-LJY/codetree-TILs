c29ef893b9c87d1e5ac7cc55a22fad06043117b3
Account-LJY <138774652+Account-LJY@users.noreply.github.com>
2023-12-18 18:42:32 +0900

    Title: 그 계절, 그 날, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 231218/그 계절, 그 날/that-season-that-day.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int many[7] = {1, 3, 5, 7, 8, 10, 12};
+int small[5] = {2, 4, 6, 9, 11};
+
+bool aaa(int a) {
+    if (a % 4 == 0) {
+        if (a % 100 == 0) {
+            if (a % 400 == 0) {
+                return true;
+            }
+            return false;
+        }
+        return true;
+    } else {
+        return false;
+    }
+}
+
+bool judge(int a, int b, int c) {
+    auto it = find(begin(many), end(many), b);
+    if (it != end(many)) {
+        if (b <= 31) {
+            return true;
+        }
+    } else {
+        if (a == 4 || a == 6 || a == 9 || a == 11) {
+            if (b <= 30) {
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            if (b <= 29 && aaa(a)) {
+                return true;
+            } else if (b <= 30 && (!aaa(a))) {
+                return true;
+            } else {
+                return true;
+            }
+        }
+    }
+}
+
+void season(int a) {
+    if (a >= 3 && a <= 5) {
+        cout << "Spring";
+    } else if(a >= 6 && a <= 8) {
+        cout << "Summer";
+    } else if(a >= 9 && a <= 11) {
+        cout << "Fall";
+    } else {
+        cout << "Winter";
+    }
+}
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int Y, M, D;
+    cin >> Y >> M >> D;
+    if(judge(Y, M, D)) {
+        season(M);
+    } else {
+        cout << -1;
+    }
+    return 0;
+}
