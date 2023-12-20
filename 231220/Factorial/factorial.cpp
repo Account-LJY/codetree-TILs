8c29391297212ee3b20dd4447a8529cc5b615d43
Account-LJY <138774652+Account-LJY@users.noreply.github.com>
2023-12-20 15:58:41 +0900

    Title: Factorial, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231220/Factorial/factorial.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int n;
+
+int aaa(int a) {
+    if (a == 1) {
+        return 1;
+    }
+    return a * aaa(a - 1);
+}
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    cin >> n;
+    cout << aaa(n);
+    return 0;
+}
