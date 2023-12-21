2d5a37ba1a9065ce2ecc6aff51fd20d916bc24ce
Account-LJY <138774652+Account-LJY@users.noreply.github.com>
2023-12-21 23:05:56 +0900

    Title: 2개씩 그룹짓기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 231221/2개씩 그룹짓기/group-of-pairs.cpp
+++ 231221/2개씩 그룹짓기/group-of-pairs.cpp
@@ -9,7 +9,7 @@ int arr[1000];
 int main() {
     // 여기에 코드를 작성해주세요.
     cin >> n;
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < 2 * n; i++) {
         cin >> arr[i];
     }
     sort(arr, arr + n);
