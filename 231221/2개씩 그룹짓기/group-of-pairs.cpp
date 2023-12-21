0bf85c2e66cd8927eede0e723dbd412218f4382f
Account-LJY <138774652+Account-LJY@users.noreply.github.com>
2023-12-21 23:29:00 +0900

    Title: 2개씩 그룹짓기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 231221/2개씩 그룹짓기/group-of-pairs.cpp
+++ 231221/2개씩 그룹짓기/group-of-pairs.cpp
@@ -9,7 +9,7 @@ int n;
 int main() {
     // 여기에 코드를 작성해주세요.
     cin >> n;
-    int nums[1000];
+    int nums[2000];
 
     for(int i = 0; i < 2 * n; i++) {
         cin >> nums[i];
@@ -19,7 +19,7 @@ int main() {
 
     int group_max = 0;
     for(int i = 0; i < n; i++) {
-        int group_sum = nums[i] + nums[2*n - 1 - i];
+        int group_sum = nums[i] + nums[2 * n - 1 - i];
         if(group_sum > group_max)
             group_max = group_sum;
     }
