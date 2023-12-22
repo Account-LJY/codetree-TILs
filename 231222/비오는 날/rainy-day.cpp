8203416163ea48bbe2209d31bee347f00a76a469
Account-LJY <138774652+Account-LJY@users.noreply.github.com>
2023-12-22 23:21:31 +0900

    Title: 비오는 날, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 231222/비오는 날/rainy-day.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+class Info {
+    public:
+        string date;
+        string day;
+        string weather;
+        Info(string a = "", string b = "", string c = "") {
+            this -> date = a;
+            this -> day = b;
+            this -> weather = c;
+        }
+};
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    string a, b, c;
+    cin >> n;
+    Info info[100];
+    vector<int> vec;
+    for (int i = 0; i < n; i++) {
+        cin >> a >> b >> c;
+        info[i] = Info(a, b, c);
+    }
+    for (int i = 0; i < n; i++) {
+        if (info[i].weather == "Rain") {
+            vec.push_back(i);
+        }
+    }
+    
+    int idx = vec[0];
+    for (int i = 1; i < vec.size(); i++) {
+        if (info[idx].date > info[vec[i]].date) {
+            idx = i;
+        }
+    }
+
+    cout << info[idx].date << " " << info[idx].day << " " << info[idx].weather;
+    return 0;
+}
