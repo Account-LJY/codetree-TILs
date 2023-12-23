1e8f979defc565b014d5bb7ec29d76d5ec93f748
Account-LJY <138774652+Account-LJY@users.noreply.github.com>
2023-12-23 15:39:29 +0900

    Title: 개인정보, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 231223/개인정보/personal-info.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+class Info {
+    public: 
+        string name;
+        int height;
+        float weight;
+        Info(string a, int b, float c) {
+            this -> name = a;
+            this -> height = b;
+            this -> weight = c;
+        }
+        Info() {}
+};
+
+bool cmp1(Info a, Info b) {
+    return a.name < b.name;
+}
+
+bool cmp2(Info a, Info b) {
+    return a.height > b.height;
+}
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    Info info[5];
+    for (int i = 0; i < 5; i++) {
+        string a;
+        int b;
+        float c;
+        cin >> a >> b >> c;
+        info[i] = Info(a, b, c);
+    }
+    sort(info, info + 5, cmp1);
+
+    cout << "name" << endl;
+    for (int i = 0; i < 5; i++) {
+        cout << info[i].name << " " << info[i].height << " " << info[i].weight << endl;
+    }
+
+    sort(info, info + 5, cmp2);
+    cout << "\nheight" << endl;
+    for (int i = 0; i < 5; i++) {
+        cout << info[i].name << " " << info[i].height << " " << info[i].weight << endl;
+    }
+    return 0; 
+}
