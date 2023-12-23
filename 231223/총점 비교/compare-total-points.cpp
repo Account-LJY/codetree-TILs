8a75a8bed1217e026e8b197fbb6af8773e8ba580
Account-LJY <138774652+Account-LJY@users.noreply.github.com>
2023-12-23 13:02:08 +0900

    Title: 총점 비교, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231223/총점 비교/compare-total-points.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+class Info {
+    public:
+        int kor, eng, mat;
+        string name;
+        Info(string a = "", int b = 0, int c = 0, int d = 0) {
+            this -> name = a;
+            this -> kor = b;
+            this -> eng = c;
+            this -> mat = d;
+        }
+};
+
+bool cmp(Info a, Info b) {
+    return a.kor + a.eng + a.mat < b.kor + b.eng + b.mat;
+}
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    cin >> n;
+    Info info[10];
+    for (int i = 0; i < n; i++) {
+        string a;
+        int b, c, d;
+        cin >> a >> b >> c >> d;
+        info[i] = Info(a, b, c, d);
+    }
+    sort(info, info + n, cmp);
+    for (int i = 0; i < n; i++) {
+        cout << info[i].name << " " << info[i].kor << " " << info[i].eng << " " << info[i].mat << endl;
+    }
+    return 0;
+}
