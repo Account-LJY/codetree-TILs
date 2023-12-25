6fda4f3ea0cf6d4eb5e7dc96d251922c43182192
Account-LJY <138774652+Account-LJY@users.noreply.github.com>
2023-12-26 00:04:09 +0900

    Title: 흰검 칠하기, Time: 11ms, Memory: 7MB, Status: Wrong Answer - CodeTree

---
+++ 231226/흰검 칠하기/painting-white-black.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <vector>
+#include <tuple>
+
+using namespace std;
+
+vector<pair<int, string>> vec(200001, make_pair(0, ""));
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    cin >> n;
+    int start = 100000;
+    int Max = -1;
+    int Min = 200001;
+    for (int j = 0; j < n; j++) {
+        int r;
+        string dir;
+        cin >> r >> dir;
+        if (dir == "R") {
+            for (int i = start; i < start + r; i++) {
+                if(i == start && vec[i].second == "black") {
+                    continue;
+                }
+                vec[i].first++;
+                vec[i].second = "black";
+            }
+            start += (r - 1);
+            if (start > Max) {
+                Max = start;
+            }
+        } else {
+            for (int i = start - r + 1; i <= start; i++) {
+                if(i == start && vec[i].second == "white") {
+                    continue;
+                }
+                vec[i].first++;
+                vec[i].second = "white";
+            }
+            start -= (r - 1);
+            if (Min > start) {
+                Min = start;
+            }
+        }
+    }
+    int white = 0;
+    int black = 0;
+    int gray = 0;
+    int cnt = 0;
+    string color;
+    for (int i = Min; i <= Max; i++) {
+        cnt = vec[i].first;
+        color = vec[i].second;
+        if (cnt >= 4) {
+            gray++;
+        } else {
+            if (color == "white") {
+                white++;
+            } else {
+                black++;
+            }
+        }
+    }
+    cout << white << " " << black << " " << gray;
+    return 0;
+}
