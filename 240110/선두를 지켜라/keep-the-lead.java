3b77bd6ca53e53542690f37b1128b450bf099b7b
Account-LJY <138774652+Account-LJY@users.noreply.github.com>
2024-01-10 17:51:54 +0900

    Title: 선두를 지켜라, Time: 220ms, Memory: 27MB, Status: Wrong Answer - CodeTree

--- 240110/선두를 지켜라/keep-the-lead.java
+++ 240110/선두를 지켜라/keep-the-lead.java
@@ -2,16 +2,16 @@ import java.util.Scanner;
 
 public class Main {
     public static int n, m, v, t;
-    public static int[] A, B;
+    public static int[] A, B, C;
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         n = sc.nextInt();
         m = sc.nextInt();
 
-        A = new int[1000];
-        B = new int[1000];
-//        C = new int[1000]; // 거리 차이
+        A = new int[1000000];
+        B = new int[1000000];
+        C = new int[1000000]; // 거리 차이
 
         int timeA = 1;
         for (int i = 0; i < n; i++) {
@@ -33,19 +33,13 @@ public class Main {
             }
         }
 
-//        for (int i = 0; i < timeB; i++) {
-//            C[i] = A[i] - B[i];
-//        }
-//
-//        int change = 0;
-//        for (int i = 1; i < timeB; i++) {
-//            if(C[i] > 0 && C[i - 1] <= 0 || C[i] < 0 && C[i - 1] >= 0) {
-//                change++;
-//            }
-//        }
+        for (int i = 0; i < timeB; i++) {
+            C[i] = A[i] - B[i];
+        }
+
         int change = 0;
         for (int i = 1; i < timeB; i++) {
-            if((A[i] - B[i]) > 0 && (A[i - 1] - B[i - 1]) <= 0 || (A[i] - B[i]) < 0 && (A[i - 1] - B[i - 1]) >= 0) {
+            if(C[i] > 0 && C[i - 1] <= 0 || C[i] < 0 && C[i - 1] >= 0) {
                 change++;
             }
         }
