af97fa34bf13f1720cde9817f465bde7053896b9
Account-LJY <138774652+Account-LJY@users.noreply.github.com>
2024-01-10 17:43:34 +0900

    Title: 선두를 지켜라, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240110/선두를 지켜라/keep-the-lead.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+
+public class Main {
+    public static int n, m, v, t;
+    public static int[] A, B, C;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        n = sc.nextInt();
+        m = sc.nextInt();
+
+        A = new int[1000];
+        B = new int[1000];
+        C = new int[1000]; // 거리 차이
+
+        int timeA = 1;
+        for (int i = 0; i < n; i++) {
+            v = sc.nextInt();
+            t = sc.nextInt();
+            for (int j = 1; j <= t; j++) {
+                A[timeA] = A[timeA - 1] + v;
+                timeA++;
+            }
+        }
+
+        int timeB = 1;
+        for (int i = 0; i < m; i++) {
+            v = sc.nextInt();
+            t = sc.nextInt();
+            for (int j = 1; j <= t; j++) {
+                B[timeB] = B[timeB - 1] + v;
+                timeB++;
+            }
+        }
+
+        for (int i = 0; i < timeB; i++) {
+            C[i] = A[i] - B[i];
+        }
+
+        int change = 0;
+        for (int i = 1; i < timeB; i++) {
+            if(C[i] > 0 && C[i - 1] <= 0 || C[i] < 0 && C[i - 1] >= 0) {
+                change++;
+            }
+        }
+        System.out.println(change - 1);
+    }
+}
