33de4dbbab8e5c394d7efc9406e5dcc89c8e3818
Account-LJY <138774652+Account-LJY@users.noreply.github.com>
2024-01-10 17:48:14 +0900

    Title: 선두를 지켜라, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240110/선두를 지켜라/keep-the-lead.java
+++ 240110/선두를 지켜라/keep-the-lead.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Main {
     public static int n, m, v, t;
-    public static int[] A, B, C;
+    public static int[] A, B;
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
@@ -11,7 +11,7 @@ public class Main {
 
         A = new int[1000];
         B = new int[1000];
-        C = new int[1000]; // 거리 차이
+//        C = new int[1000]; // 거리 차이
 
         int timeA = 1;
         for (int i = 0; i < n; i++) {
@@ -33,13 +33,19 @@ public class Main {
             }
         }
 
-        for (int i = 0; i < timeB; i++) {
-            C[i] = A[i] - B[i];
-        }
-
+//        for (int i = 0; i < timeB; i++) {
+//            C[i] = A[i] - B[i];
+//        }
+//
+//        int change = 0;
+//        for (int i = 1; i < timeB; i++) {
+//            if(C[i] > 0 && C[i - 1] <= 0 || C[i] < 0 && C[i - 1] >= 0) {
+//                change++;
+//            }
+//        }
         int change = 0;
         for (int i = 1; i < timeB; i++) {
-            if(C[i] > 0 && C[i - 1] <= 0 || C[i] < 0 && C[i - 1] >= 0) {
+            if((A[i] - B[i]) > 0 && (A[i - 1] - B[i - 1]) <= 0 || (A[i] - B[i]) < 0 && (A[i - 1] - B[i - 1]) >= 0) {
                 change++;
             }
         }
